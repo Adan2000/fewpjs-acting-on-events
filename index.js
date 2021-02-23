92021e62b0ba88c403cc40c9e389d8f558998e3b
Adan Escamilla <adanescamilla580@gmail.com>
2021-02-22 23:38:25 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Your code here
+
+
+
+//   document.addEventListener("keydown", function(e) {
+//     if (e.key === "ArrowLeft") {
+//       let leftNumbers = dodger.style.left.replace("px", "");
+//       let left = parseInt(leftNumbers, 10);
+  
+//       dodger.style.left = `${left - 1}px`;
+//     }
+//   });
+
+let dodger = document.getElementById("dodger"); 
+
+dodger.style.backgroundColor = "#FF69B4";
+
+function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 10}px`;
+    }
+  }
+
+  function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 10}px`;
+    }
+  } 
+
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+    else { 
+    (e.key === "ArroyLeft") 
+        moveDodgerLeft();
+    }
+  }); 
